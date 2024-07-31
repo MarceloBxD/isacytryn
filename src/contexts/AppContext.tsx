830c2fd61cf88a94ef68855a5084fb3ee7eff10b
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-07-31 17:37:19 -0300

    refactor: Update image path for About section and optimize responsive layout in AppContext

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,22 +1,65 @@
 "use client";
 
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect } from "react";
 
 type AppContextProps = {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
 };
 
 const AppContext = createContext<AppContextProps | undefined>(undefined);
 
+type DeviceProps = {
+  mobile: boolean | undefined;
+  tablet: boolean | undefined;
+  desktop: boolean | undefined;
+};
+
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [device, setDevice] = React.useState<DeviceProps>({
+    mobile: undefined,
+    tablet: undefined,
+    desktop: undefined,
+  });
 
-  return (
-    <AppContext.Provider value={{ isOpen, setIsOpen }}>
-      {children}
-    </AppContext.Provider>
-  );
+  const [isMobile, setIsMobile] = React.useState(false);
+  const [isTablet, setIsTablet] = React.useState(false);
+  const [isDesktop, setIsDesktop] = React.useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 640) {
+        setIsMobile(true);
+        setIsTablet(false);
+        setIsDesktop(false);
+      } else if (window.innerWidth > 640 && window.innerWidth <= 1024) {
+        setIsMobile(false);
+        setIsTablet(true);
+        setIsDesktop(false);
+      } else {
+        setIsMobile(false);
+        setIsTablet(false);
+        setIsDesktop(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isMobile, isTablet, isDesktop, setIsMobile, setIsTablet, setIsDesktop]);
+
+  const value = {
+    isOpen,
+    setIsOpen,
+    isMobile,
+    isTablet,
+    isDesktop,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useApp = () => {
--- src/sections/Presentation/index.tsx
+++ src/sections/Presentation/index.tsx
@@ -10,57 +10,20 @@ import { CtaButton } from "../../../components/CtaButton";
 // animation
 import { FADE_IN_BASIC_ANIMATION } from "@/utils/animations";
 import Image from "next/image";
-// swiper
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// swiper modules
-import { Pagination, Autoplay } from "swiper/modules";
 
 // data
 import { PRESENTATION_DATA } from "@/data";
 
-const IMAGES_TO_SLIDE = [
-  "/images/bg.jpg",
-  "/images/bg.jpg",
-  "/images/bg.jpg",
-  "/images/bg.jpg",
-];
-
-const SwiperComponent: React.FC = () => {
+const PresentationImage: React.FC = () => {
   return (
-    <Swiper
-      className="h-[calc(100vh-85px)] max-w-screen md:h-[calc(100vh-65.6px)] custom-swiper"
-      pagination={{
-        clickable: true,
-        enabled: true,
-        dynamicBullets: true,
-      }}
-      loop={true}
-      modules={[Pagination, Autoplay]}
-      parallax={true}
-      autoplay={{
-        delay: 5000,
-        disableOnInteraction: false,
-      }}
-      slidesPerView={1}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-      // fix pagination on mobile
-    >
-      {IMAGES_TO_SLIDE.map((image, index) => (
-        <SwiperSlide key={index}>
-          <div className="h-screen w-screen relative">
-            <Image
-              src={image}
-              alt="Picture of the author"
-              layout="fill"
-              objectFit="cover"
-            />
-            <div className="absolute inset-0 bg-black opacity-70"></div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative w-full h-[calc(100vh-80px)] md:h-full md:w-1/3">
+      <Image
+        layout="fill"
+        alt="Isa Front Image"
+        className="object-cover object-top"
+        src="/images/isa-front-image.jpg"
+      />
+    </div>
   );
 };
 
@@ -68,18 +31,10 @@ const Presentation: React.FC = () => {
   return (
     <section
       id="apresentacao"
-      className="relative flex flex-col md:flex-row items-center md:h-[calc(100vh-80px)] max-w-screen -mb-10 md:mb-12"
+      className="relative flex flex-col-reverse md:flex-row items-center md:h-[calc(100vh-80px)] max-w-screen -mb-10 md:mb-12"
     >
-      <div className="relative w-full max-w-screen h-[280px] md:w-[1400px]  md:h-full">
-        <Image
-          layout="fill"
-          alt="Isa Front Image"
-          className="object-cover object-top bg-center bg-cover "
-          src="/images/bg.jpg"
-        />
-      </div>
-      <div className="md:pl-12 mt-5 max-w-[80%] md:w-full ">
-        <div className="flex relative flex-col items-left text-quaternary gap-2 md:gap-4">
+      <div className="mt-5 flex-1 md:w-2/3 flex flex-col justify-center items-center md:items-start px-4 md:px-8">
+        <div className="flex flex-col items-center md:items-start text-quaternary gap-2 md:gap-4">
           <motion.h1
             {...FADE_IN_BASIC_ANIMATION}
             className="text-lg text-center md:text-left md:text-3xl"
@@ -88,74 +43,33 @@ const Presentation: React.FC = () => {
           </motion.h1>
           <motion.h2
             {...FADE_IN_BASIC_ANIMATION}
-            className="text-lg md:text-2xl font-bold text-center md:text-left "
+            className="text-lg md:text-2xl font-bold text-center md:text-left"
           >
             {PRESENTATION_DATA.specialty}
           </motion.h2>
         </div>
         <motion.p
           {...FADE_IN_BASIC_ANIMATION}
-          className="text-sm text-quaternary  text-center md:text-left relative md:max-w-[50%]  py-3 md:py-5"
+          className="text-sm text-quaternary max-w-[70%] mx-auto md:mx-0 md:max-w-[50%] text-center md:text-left py-3 md:py-5"
         >
           {PRESENTATION_DATA.description}
         </motion.p>
         <motion.p
           {...FADE_IN_BASIC_ANIMATION}
-          className="text-sm text-quaternary relative md:max-w-[45%] text-center md:text-left pb-3 md:pb-5 font-semibold"
+          className="text-sm text-quaternary max-w-[70%] mx-auto md:mx-0 md:max-w-[45%] text-center md:text-left pb-3 md:pb-5 font-semibold"
         >
           {PRESENTATION_DATA.preCta}
         </motion.p>
         <motion.div
           {...FADE_IN_BASIC_ANIMATION}
-          className="flex text-left mx-auto md:mx-0 w-fit justify-center md:justify-left"
+          className="flex justify-center md:justify-start w-fit"
         >
           <CtaButton title="Entre em contato pelo <b>WhatsApp</b>!" />
         </motion.div>
       </div>
+      <PresentationImage />
     </section>
   );
 };
 
 export default Presentation;
-
-{
-  /* <div className="absolute inset-0 flex flex-col gap-5 pt-16 md:pt-40 justify-left container mx-auto z-10">
-        <div className="relative h-[380px] w-[200px]">
-          <Image
-            layout="fill"
-            alt="Isa Front Image"
-            className="object-cover"
-            src="/images/isa-front-image.jpg"
-          />
-        </div>
-        <div className="flex relative flex-col items-left text-white gap-3">
-          <motion.h1
-            {...FADE_IN_BASIC_ANIMATION}
-            className="text-3xl text-left"
-          >
-            {PRESENTATION_DATA.degree}
-          </motion.h1>
-          <motion.h2
-            {...FADE_IN_BASIC_ANIMATION}
-            className="text-2xl font-bold text-left"
-          >
-            {PRESENTATION_DATA.specialty}
-          </motion.h2>
-        </div>
-        <motion.p
-          {...FADE_IN_BASIC_ANIMATION}
-          className="text-lg text-white relative md:max-w-[50%] text-left"
-        >
-          {PRESENTATION_DATA.description}
-        </motion.p>
-        <motion.p
-          {...FADE_IN_BASIC_ANIMATION}
-          className="text-lg text-white relative md:max-w-[45%] text-left font-semibold"
-        >
-          {PRESENTATION_DATA.preCta}
-        </motion.p>
-        <motion.div {...FADE_IN_BASIC_ANIMATION} className="flex justify-left">
-          <CtaButton title="Entre em contato pelo <b>Whatsapp</b>!" />
-        </motion.div>
-      </div> */
-}
