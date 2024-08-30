9c3eaa314cc3916dc2f59130a4c1447b5051ea5c
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-08-30 12:56:07 -0300

    refactor: Update image paths and optimize layout in Presentation component

--- src/data/presentation-data/index.tsx
+++ src/data/presentation-data/index.tsx
@@ -1,12 +1,22 @@
 const PRESENTATION_DATA = {
-  degree: "Psicóloga clínica especializada em adultos",
-  specialty: "Terapia cognitivo-comportamental",
-  description: `
-      Talvez você esteja enfrentando alguma dificuldade que está afetando sua capacidade de pensar com clareza, resolver problemas ou mesmo sentir prazer. Se algo está causando sofrimento em sua vida, isso merece atenção e eu estou aqui para ajudar.
-    `,
+  degree: <>Psicóloga clínica especializada em adultos"</>,
+  specialty: <>Terapia cognitivo-comportamental</>,
+  description: (
+    <>
+      Talvez você esteja enfrentando alguma dificuldade que está afetando sua
+      capacidade de pensar com clareza, resolver problemas ou mesmo sentir
+      prazer.
+      <br /> Se algo está causando sofrimento em sua vida, isso merece atenção e
+      eu estou aqui para ajudar.
+    </>
+  ),
 
-  preCta:
-    "Dê uma chance para si mesmo e marque uma sessão de terapia. Nunca é tarde para (re)começar.",
+  preCta: (
+    <>
+      Dê uma chance para si mesmo e marque uma sessão de terapia. Nunca é tarde
+      para (re)começar.
+    </>
+  ),
   bgImg: "/images/bg.webp",
 };
 
